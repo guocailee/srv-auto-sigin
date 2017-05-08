9e290c880ebd28b99c9417af43e7330ed1d95ba7
gagelee.li <gagelee.li@zkteco.com>
2017-05-08 13:09:36 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 'use strict';
 var router = require('express').Router();
 router.use('/todos', require('./todos.js'))
-
+router.use('/signin', require('./signin.js'))
 module.exports = router;
--- routes/signin.js
+++ routes/signin.js
@@ -1,4 +1,74 @@
-function getUrl(username) {
-  let url = 's1.zkcserv.com/cb_hrms/index.cfm?event=ionicAction.ionicAction.signInAction&_user_name=' + username + '&_pass_word=A5DD89CC477FE32B264C41CA561D2BE8&_is_login=1&_notification_token=13065ffa4e07eebb75b&_device_type=android'
+var request = require('request')
+var md5 = require('js-md5')
+var router = require('express').Router();
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function getUrl(username) {
+  var url = 'http://s1.zkcserv.com/cb_hrms/index.cfm?event=ionicAction.ionicAction.signInAction&_user_name=' + username + '&_pass_word=A5DD89CC477FE32B264C41CA561D2BE8&_is_login=1&_notification_token=13065ffa4e07eebb75b&_device_type=android'
+  return url
+}
+
+function getLatitude() {
+  var origin = 24.612064;
+  return origin + getRandomInt(1, 100) * 0.000001;
+}
+
+function getLongitude() {
+  var origin = 118.048888;
+  return origin + getRandomInt(1, 100) * 0.000001;
+}
+
+function getAddress() {
+  var addrs = ['福建省厦门市集美区集美北大道', '集美北大道靠近集美软件园(地铁站)', '', '福建省厦门市集美区G15(沈海高速)', '集美北大道靠近集美软件园B区B02'];
+  return addrs[getRandomInt(0, addrs.length - 1)]
+}
+
+function getCardRecTime(value) {
+  var formats = value.split(':');
+  var getRandomMinutes = getRandomInt(1, 15);
+  var getRandomSeconds = getRandomInt(1, 60);
+  if (getRandomSeconds < 10) {
+    getRandomSeconds = '0' + getRandomSeconds;
+  }
+  if (formats[0] === '08') {
+    formats[1] = parseInt(formats[1]) - getRandomMinutes;
+  } else {
+    formats[1] = parseInt(formats[1]) + getRandomMinutes;
+  }
+  if (formats[1] < 10) {
+    formats[1] = '0' + formats[1];
+  }
+  formats[2] = getRandomSeconds;
+  return formats.join(':')
+}
+
+function getToken(username) {
+  var randomSeed = md5(username)
+  return randomSeed.substring(6, 22)
+}
+
+router.post('/', function (req, res) {
+  var userInfo = req.body
+  request.post({
+    url: getUrl(userInfo.phone),
+    form: {
+      latitude: getLatitude(),
+      longitude: getLongitude(),
+      address: getAddress(),
+      wifi_value: '',
+      wifi_mac: '',
+      card_rec_time: getCardRecTime(userInfo.value),
+      mobile_token: getToken(userInfo.phone)
+    }
+  }, function (err, httpResponse, body) {
+    console.log(httpResponse.body)
+    res.json(httpResponse.body)
+  })
+})
+
+module.exports = router;
