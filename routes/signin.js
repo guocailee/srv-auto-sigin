596d1d110bc37a5c9136ac06be09369a6cb2fb61
gagelee.li <gagelee.li@zkteco.com>
2018-04-23 18:05:01 +0800

    update

--- routes/signin.js
+++ routes/signin.js
@@ -12,7 +12,7 @@ function getRandomInt(min, max) {
 }
 
 function getUrl(username) {
-  var url = 'http://s1.zkcserv.com/cb_hrms/index.cfm?event=ionicAction.ionicAction.signInAction&_user_name=' + username + '&_pass_word=A5DD89CC477FE32B264C41CA561D2BE8&_is_login=1&_notification_token=13065ffa4e07eebb75b&_device_type=android'
+  var url = 'http://zkteco.zkcserv.com/cb_hrms/index.cfm?event=ionicAction.ionicAction.signInAction&_user_name=' + username + '&_pass_word=A5DD89CC477FE32B264C41CA561D2BE8&_is_login=1&_notification_token=13065ffa4e07eebb75b&_device_type=android'
   return url
 }
 
